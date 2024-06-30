8d309fe6af687e64801a87b114e009905ca8ddea
mehdi-cloud <mehdikamal1999@gmail.com>
2024-06-30 14:02:41 -0700

    fingers crossed, fixes deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import MetricsTable from './components/MetricsTable.ts';
+import MetricsTable from './components/MetricsTable';
 import NavBar from './components/NavBar';
 import { ProductTab } from './components/ProductTab';
 
